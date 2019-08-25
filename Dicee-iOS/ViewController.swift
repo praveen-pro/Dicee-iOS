e996aafc981a1070ce76dbcbea63e7fae5f9edac
praveen <praveenj.pro@gmail.com>
2019-08-25 14:18:51 +0530

    Initial Commit

---
+++ Dicee-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee-iOS
+//
+//  Created by Praveen J on 25/08/19.
+//  Copyright © 2019 Praveen J. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
